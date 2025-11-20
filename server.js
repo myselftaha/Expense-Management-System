60f63a93b0b62d30983d98ff0e1fbece6f2f100c
TahaJameel <mrtaha09876@gmail.com>
2025-11-20 23:37:57 +0500

    neww

--- server.js
+++ server.js
@@ -17,18 +17,17 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+// 1. API Routes FIRST
 app.use('/users', require('./routes/userRoute'));
 app.use('/transections', require('./routes/transectionRoutes'));
 
-// Static files
+// 2. Static Files SECOND
 app.use(express.static(path.join(__dirname, './client/build')));
 
-// --- FIX IS HERE ---
-// Changed '*' to regex /(.*)/ to fix the "Missing parameter name" crash
-app.get(/(.*)/, function (req, res) {
+// 3. The Catch-All Route LAST
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-
 // --- PORT LOGIC FIX ---
 // process.env.PORT must come first for Vercel deployment
 const PORT = process.env.PORT || 8080;
