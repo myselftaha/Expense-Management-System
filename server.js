9e95e272bf9d5e463efc39765c330e52a59c1254
TahaJameel <mrtaha09876@gmail.com>
2025-11-20 22:25:49 +0500

    code add krdo

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const path = require('path');
+const connectDb = require('./config/connectDb');
+
+dotenv.config();
+
+connectDb();
+
+ 
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
+
+app.use('/users', require('./routes/userRoute'));
+
+
+app.use('/transections', require('./routes/transectionRoutes'));
+
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+const PORT = 8080 || process.env.PORT;
+
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
