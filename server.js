c04b947a359f815b24cf38bae7ac1ae7caab85e7
TahaJameel <mrtaha09876@gmail.com>
2025-11-21 14:39:37 +0500

    ssss

--- server.js
+++ server.js
@@ -1,41 +1,38 @@
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
-const colors = require("colors");
-const path = require("path");
-const connectDb = require("./config/connectDb");
-// config dot env file
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const path = require('path');
+const connectDb = require('./config/connectDb');
+
 dotenv.config();
 
-//databse call
 connectDb();
 
-//rest object
 const app = express();
 
-//middlewares
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-//routes
-//user routes
-// app.use("/api/v1/users", require("./routes/userRoute"));
-//transections routes
-app.use("/api/v1/transections", require("./routes/transectionRoutes"));
+// Routes
+app.use('/users', require('./routes/userRoute'));
+app.use('/transections', require('./routes/transectionRoutes'));
 
-//static files
-app.use(express.static(path.join(__dirname, "./client/build")));
+// Static files
+app.use(express.static(path.join(__dirname, './client/build')));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// --- FIX IS HERE ---
+// Changed '*' to regex /(.*)/ to fix the "Missing parameter name" crash
+app.get(/(.*)/, function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
-//port
-const PORT = 8080 || process.env.PORT;
+// --- PORT LOGIC FIX ---
+// process.env.PORT must come first for Vercel deployment
+const PORT = process.env.PORT || 8080;
 
-//listen server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
